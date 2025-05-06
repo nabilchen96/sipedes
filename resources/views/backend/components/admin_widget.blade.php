dd8d57368f579b3feff7a92f427832f7a40480c7
nabilchen96 <escendol900@gmail.com>
2025-05-06 20:52:09 +0700

    update

--- resources/views/backend/components/admin_widget.blade.php
+++ resources/views/backend/components/admin_widget.blade.php
@@ -13,7 +13,7 @@
                 <img src="https://themewagon.github.io/purple-react/static/media/circle.953c9ca0.svg"
                     class="card-img-absolute" alt="circle">
                 <h4 class="font-weight-normal mb-3">
-                    Pegawai BKPSDM
+                    Total Pegawai Terdaftar
                     <i class="bi bi-person-circle float-right"></i>
                 </h4>
                 <h2>
